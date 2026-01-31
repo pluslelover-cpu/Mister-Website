51e44c228dfb50b3d9d45de31c4eefcf10e6a4cb
misterdatos-official-website <pluslelover@gmail.com>
2026-01-31 06:33:00 -0600

    rediseño adaptado a moviles

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 window.addEventListener('scroll', function() {
     let navbar = document.getElementById("navbar");
-    let seccionInicio = document.getElementById("start");
+    let startSection = document.getElementById("start");
     
-    let inicioRect = seccionInicio.getBoundingClientRect();
+    let startRect = startSection.getBoundingClientRect();
 
-    if (inicioRect.bottom > 0) {
+    if (startRect.bottom > 0) {
         navbar.style.opacity = "1";
         navbar.style.pointerEvents = "auto";
     } else {
         navbar.style.opacity = "0";
         navbar.style.pointerEvents = "none";
     }
+});
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menuContainer = document.querySelector('.menu-container');
+
+hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('active');
+    menuContainer.classList.toggle('active');
+});
+
+window.addEventListener('load', () => {
+    const loaderWrapper = document.getElementById('loader-wrapper');
+    const contentWrapper = document.getElementById('content-wrapper');
+
+    loaderWrapper.style.display = 'none';
+    contentWrapper.style.display = 'block';
 });
